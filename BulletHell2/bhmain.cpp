87b76eeef0fb672a64d0cc4cdc430db57d922b33
Trevor West <alexanderzero@gmail.com>
2015-10-08 18:40:18 -0700

    added glfw stuff

--- BulletHell2/bhmain.cpp
+++ BulletHell2/bhmain.cpp
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <iostream>
+
+#include "glfw_include/GLFW/glfw3.h"
 
 int main(int argc, char** argv)
 {
+	if (!glfwInit())
+	{
+		std::cout << "GLFW init failed :<" << std::endl;
+		std::cin.get();
+		return 0;
+	}
+
 	printf("hello bullet hell world!\n");
+
+	std::cin.get();
 }
