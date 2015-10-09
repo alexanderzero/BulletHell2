05ab145f06f334def7eae594a483072486e39d6d
Trevor West <alexanderzero@gmail.com>
2015-10-08 19:09:34 -0700

    hlep sean help

--- BulletHell2/Functions.h
+++ BulletHell2/Functions.h
@@ -7,4 +7,8 @@
 
 #include <string>
 
+void getOpenGLVersion(int& major, int& minor);
+void read_shaders(std::string& vert_shader, std::string& frag_shader);
 void render(GLFWwindow* window);
+void init();
+GLuint png_texture_load(const char * file_name, int * width, int * height);
